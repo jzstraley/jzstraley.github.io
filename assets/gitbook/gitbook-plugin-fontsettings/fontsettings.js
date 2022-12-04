17219596bd1f62cae4099456b0d9238798ed4cf1
jzstraley <107450982+jzstraley@users.noreply.github.com>
2022-12-04 15:34:17 -0500

    update sepia

--- assets/gitbook/gitbook-plugin-fontsettings/fontsettings.js
+++ assets/gitbook/gitbook-plugin-fontsettings/fontsettings.js
@@ -15,11 +15,6 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
             id: 0
         },
         {
-            config: 'sepia',
-            text: 'Sepia',
-            id: 1
-        },
-        {
             config: 'night',
             text: 'Night',
             id: 2
