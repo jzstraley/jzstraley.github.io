05f808616f421d467a08c9c7caf777ddb42aecf0
jzstraley <107450982+jzstraley@users.noreply.github.com>
2022-12-09 21:53:59 -0500

    c

--- assets/gitbook/gitbook-plugin-fontsettings/fontsettings.js
+++ assets/gitbook/gitbook-plugin-fontsettings/fontsettings.js
@@ -15,6 +15,11 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
             id: 0
         },
         {
+        config: 'sepia',
+        text: 'Sepia',
+        id: 1
+        },
+        {
             config: 'dark',
             text: 'Dark',
             id: 1
