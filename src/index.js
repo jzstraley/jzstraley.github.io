d5760a3852dc1e81114d671866fe249c0efa278f
jzstraley <107450982+jzstraley@users.noreply.github.com>
2022-12-18 15:37:09 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 import { hideLoginError, showLoginState, showLoginForm, showApp, showLoginError, btnLogin, btnSignup, btnLogout, txtEmail, txtPassword} from './ui'
 import { initializeApp } from 'firebase/app';
 import { getAuth, connectAuthEmulator, signInWithEmailAndPassword, onAuthStateChanged, signInWithPopup, signOut,} from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, doc, getDocs } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -43,13 +43,13 @@ const createAccount = async () => {
   const email = txtEmail.value
   const password = txtPassword.value
 
-  try {
+  //try {
     await createUserWithEmailAndPassword(auth, email, password)
-  }
-  catch(error) {
-    console.log(`There was an error: ${error}`)
-    showLoginError(error)
-  } 
+  //}
+  //catch(error) {
+    //console.log(`There was an error: ${error}`)
+   // showLoginError(error)
+  //} 
 }
 
 // Monitor auth state
@@ -58,9 +58,9 @@ const monitorAuthState = async () => {
     if (user) {
       console.log(user)
       showApp()
-      showLoginState(user)
-
       hideLoginError()
+      window.location.href = '{{site.baseurl}}/'
+      showLoginState(user)
     }
     else {
       showLoginForm()
@@ -80,107 +80,9 @@ btnLogout.addEventListener("click", logout)
 
 const auth = getAuth(firebaseApp);
 // Firebase Auth Emulator
-connectAuthEmulator(auth, "http://localhost:9099");
-
+//connectAuthEmulator(auth, "http://localhost:9099");
 monitorAuthState();
 
 
-
-
+// My edits
 const db = getFirestore(firebaseApp);
-const analytics = getAnalytics(firebaseApp);
-// Initialize firebase auth
-function initFirebaseAuth() {
-  // Listen to auth state changes.
-  onAuthStateChanged(getAuth(), authStateObserver);
-}
-
-async function signIn() {
-  // Sign in Firebase using popup auth and Google as the identity provider.
-  var provider = new GoogleAuthProvider();
-  await signInWithPopup(getAuth(), provider);
-}
-
-function signOutUser() {
-  // Sign out of Firebase.
-  signOut(getAuth());
-}
-
-// Returns the signed-in user's profile Pic URL.
-function getProfilePicUrl() {
-  return getAuth().currentUser.photoURL || '/images/profile_placeholder.png';
-}
-
-// Returns the signed-in user's display name.
-function getUserName() {
-  return getAuth().currentUser.displayName;
-}
-
-// Returns true if a user is signed-in.
-function isUserSignedIn() {
-  return !!getAuth().currentUser;
-}
-
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-function authStateObserver(user) {
-  if (user) { // User is signed in!
-    // Get the signed-in user's profile pic and name.
-    var profilePicUrl = getProfilePicUrl();
-    var userName = getUserName();
-
-    // Set the user's profile pic and name.
-    userPicElement.style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
-    userNameElement.textContent = userName;
-
-    // Show user's profile and sign-out button.
-    userNameElement.removeAttribute('hidden');
-    userPicElement.removeAttribute('hidden');
-    signOutButtonElement.removeAttribute('hidden');
-
-    // Hide sign-in button.
-    signInButtonElement.setAttribute('hidden', 'true');
-
-  } else { // User is signed out!
-    // Hide user's profile and sign-out button.
-    userNameElement.setAttribute('hidden', 'true');
-    userPicElement.setAttribute('hidden', 'true');
-    signOutButtonElement.setAttribute('hidden', 'true');
-
-    // Show sign-in button.
-    signInButtonElement.removeAttribute('hidden');
-  }
-}
-
-// Returns true if user is signed-in. Otherwise false and displays a message.
-function checkSignedInWithMessage() {
-  // Return true if the user is signed in Firebase
-  if (isUserSignedIn()) {
-    return true;
-  }
-}
-
-// Adds a size to Google Profile pics URLs.
-function addSizeToGoogleProfilePic(url) {
-  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
-    return url + '?sz=150';
-  }
-  return url;
-}
-
-// Shortcuts to DOM Elements.
-var submitButtonElement = document.getElementById('submit');
-var mediaCaptureElement = document.getElementById('mediaCapture');
-var userPicElement = document.getElementById('user-pic');
-var userNameElement = document.getElementById('user-name');
-var signInButtonElement = document.getElementById('sign-in');
-var signOutButtonElement = document.getElementById('sign-out');
-var signInSnackbarElement = document.getElementById('must-signin-snackbar');
-
-// Detect Auth State
-onAuthStateChanged(auth, user => {
-  if(user != null) {
-    console.log('Logged in!');
-  } else {
-    console.log('No user');
-  }
-}); 
