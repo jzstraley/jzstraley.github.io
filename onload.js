4a6373bdec426968cae2c4b1aa58b02a7103b6b7
jzstraley <107450982+jzstraley@users.noreply.github.com>
2023-03-18 20:45:18 -0400

    2nd update

---
+++ onload.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        connectParticles: 'True',
+        speed: '0.3',
+        color: '#48F2E3',
+        sizeVariations: '1',
+        maxParticles: '100'
+    });
+};
