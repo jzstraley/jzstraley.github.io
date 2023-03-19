f77a2b58dd81bddd282f71c783ddf0da37e96c8a
jzstraley <107450982+jzstraley@users.noreply.github.com>
2023-03-19 02:32:46 -0400

    Major index update

--- assets/coverpage/onload.js
+++ assets/coverpage/onload.js
@@ -2,8 +2,8 @@ window.onload = function() {
     Particles.init({
         selector: '.background',
         connectParticles: 'True',
-        speed: '0.3',
-        color: '#48F2E3',
+        speed: '0.4',
+        color: '#363539',
         sizeVariations: '1',
         maxParticles: '100'
     });
