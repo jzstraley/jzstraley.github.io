af4c67f3b3bde3d31045bfe28f0df5332647a178
jzstraley <107450982+jzstraley@users.noreply.github.com>
2023-03-18 20:34:20 -0400

    Update

--- demo/js/demo.js
+++
@@ -1,9 +0,0 @@
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-}, true);
-
